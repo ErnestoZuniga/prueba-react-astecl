dbbd80af767d8d0ceac4eb9d41c98d4d6697eb1a
unknown <wandern.dechecs@gmail.com>
2022-02-10 15:57:32 -0600

    Feat: Cambios en los estilos de la tabla

--- src/custom components/tableComponent.jsx
+++ src/custom components/tableComponent.jsx
@@ -13,40 +13,42 @@ const TableComponent = (props) => {
   const data = props.data
   return (
     <>
-    {<table className={styles.table}>
-      <tr>
-        {Object.keys(data[1]).map( entry => {
-          return <th>{entry}</th>
-        })
+    <div className={styles.container}>
+      {<table className={styles.table}>
+        <tr>
+          {Object.keys(data[1]).map( entry => {
+            return <th>{entry}</th>
+          })
+          }
+        </tr>
+        {
+          data.map( (value, index) => {
+            return (
+                <tr  key={index} >
+                  <td className={styles.col__light}>{value._id}</td>
+                  <td className={styles.col__dark}>{value.cityid}</td>
+                  <td className={styles.col__light}>{value.validdateutc}</td>
+                  <td className={styles.col__dark}>{value.winddirectioncardinal}</td>
+                  <td className={styles.col__light}>{value.probabilityofprecip}</td>
+                  <td className={styles.col__dark}>{value.relativehumidity}</td>
+                  <td className={styles.col__light}>{value.name}</td>
+                  <td className={styles.col__dark}>{value["date-insert"]}</td>
+                  <td className={styles.col__light}>{value.longitude}</td>
+                  <td className={styles.col__dark}>{value.state}</td>
+                  <td className={styles.col__light}>{value.lastreporttime}</td>
+                  <td className={styles.col__dark}>{value.skydescriptionlong}</td>
+                  <td className={styles.col__light}>{value.stateabbr}</td>
+                  <td className={styles.col__dark}>{value.tempc}</td>
+                  <td className={styles.col__light}>{value.latitude}</td>
+                  <td className={styles.col__dark}>{value.iconcode}</td>
+                  <td className={styles.col__light}>{value.windspeedkm}</td>
+                </tr>
+            )
+          })
         }
-      </tr>
-      {
-        data.map( (value, index) => {
-          return (
-              <tr  key={index} >
-                <td className={styles.col__light}>{value._id}</td>
-                <td className={styles.col__dark}>{value._cityid}</td>
-                <td className={styles.col__light}>{value.validdateutc}</td>
-                <td className={styles.col__dark}>{value.winddirectioncardinal}</td>
-                <td className={styles.col__light}>{value.probabilityofprecip}</td>
-                <td className={styles.col__dark}>{value.relativehumidity}</td>
-                <td className={styles.col__light}>{value.name}</td>
-                <td className={styles.col__dark}>{value["date-insert"]}</td>
-                <td className={styles.col__light}>{value.longitude}</td>
-                <td className={styles.col__dark}>{value.state}</td>
-                <td className={styles.col__light}>{value.lastreporttime}</td>
-                <td className={styles.col__dark}>{value.skydescriptionlong}</td>
-                <td className={styles.col__light}>{value.stateabbr}</td>
-                <td className={styles.col__dark}>{value.tempc}</td>
-                <td className={styles.col__light}>{value.latitude}</td>
-                <td className={styles.col__dark}>{value.iconcode}</td>
-                <td className={styles.col__light}>{value.windspeedkm}</td>
-              </tr>
-          )
-        })
+      </table>
       }
-    </table>
-    }
+    </div>
     </>
   );
 }
